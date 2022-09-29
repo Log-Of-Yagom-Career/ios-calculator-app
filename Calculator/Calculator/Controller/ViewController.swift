99dc63e5d33fb23c820b88ff9c8a2f68c20089f5
dylan-yoon <d.dylany21@gmail.com>
2022-09-30 01:55:03 +0900

    feat: actionButton 생성 및 outletbutton 생성

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,9 +7,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberTextLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+
+    }
+    
+    @IBAction func zeroButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func doubleZeroButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func dotButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func operatorButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func equalButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func acButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func ceButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func signButtonTapped(_ sender: UIButton) {
+    }
+    
+
+    
 }
 
